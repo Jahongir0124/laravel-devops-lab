a70e06ae21a2538315b3a6957f1d4a350125c4fb
Jahongir0124 <jahongirrr0124@gmail.com>
2026-08-21 10:15:02 +0000

    Fix laravel route syntax

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::get('/health', function () {
 		  'status' => $healthy ? 'ok' : 'unhealthy',
 		  'app' => 'laravel-devops-lab',
 		  'database' => $database',
-		  'redis' => $redis
-			],$healthy ? 200 : 503);
+		  'redis' => $redis,
+			], $healthy ? 200 : 503);
 
 });
 
