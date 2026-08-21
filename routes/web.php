b5b5e42386c6b7f90c9f5d5d7a05fd3329d10d5d
Jahongir0124 <jahongirrr0124@gmail.com>
2026-08-21 09:42:44 +0000

    Add health endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Deploy test 15-08-2026";
 });
+Route::get('/health', function () {
+	    return response()->json([
+			'status' => 'ok',
+			'app' => 'laravel-devops-lab'
+
+]);
+
+});
